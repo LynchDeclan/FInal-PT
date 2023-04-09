5af37f2eb3f5ce7665a98113a91ac294cb724d2c
LynchDeclan <dlynch.25@acsamman.edu.jo>
2023-04-10 01:35:09 +0900

    Start of castle story

--- final_game.py
+++ final_game.py
@@ -3,6 +3,14 @@ import sys
 
 def main():
     time.sleep(2)
+    print("You awaken, feeling dizzy, in what appears to be a small room with wooden walls. After a few moments, you begin to feel a sharp, throbbing pain in your head...")
+    time.sleep(1)
+    print("A small oil lamp in the corner illuminates the room, and you see that you have been tied to a chair positioned int he center of the room...")
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
